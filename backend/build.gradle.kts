9f1661c6e9f22f025bfb3a924bc64d2f43be9dd0
Rushank <rushank@music.site>
2026-07-25 16:38:12 +0530

    security: update Netty alignment to 4.1.136.Final

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:2.4.10"))
-    implementation(platform("io.netty:netty-bom:4.1.135.Final"))
+    implementation(platform("io.netty:netty-bom:4.1.136.Final"))
     implementation("io.ktor:ktor-server-core-jvm:2.3.13")
     implementation("io.ktor:ktor-server-netty-jvm:2.3.13")
     implementation("io.ktor:ktor-server-cors-jvm:2.3.13")
